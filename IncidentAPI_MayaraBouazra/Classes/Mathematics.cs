916c720853f0a457733da6588264f1e4bc072df3
Mayara Bouwazra <mayarabouazra@gmail.com>
2026-04-26 12:53:26 +0100

    feat: add Mathematics class and add he sum method

---
+++ IncidentAPI_MayaraBouazra/Classes/Mathematics.cs
@@ -0,0 +1,10 @@
+﻿namespace IncidentAPI_MayaraBouazra.Classes
+{
+    public class Mathematics
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
